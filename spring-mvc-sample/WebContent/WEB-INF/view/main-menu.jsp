22b5caae1de8361fd1e545c5960abb9ddc310f24
Akshay <akshaybpatil.39@gmail.com>
2020-04-06 01:10:09 +0530

    First Spring MVC Project

---
+++ spring-mvc-sample/WebContent/WEB-INF/view/main-menu.jsp
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h2>Spring MVC Demo - HomePage</h2>
+<h2>WebController Output</h2>
+</body>
+</html>
---
+++ spring-mvc-sample/src/com/practice/express/WebController.java
@@ -0,0 +1,14 @@
+package com.practice.express;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+	
+	@RequestMapping("/")
+	public String showPage() {
+		return "main-menu";
+	}
+
+}
